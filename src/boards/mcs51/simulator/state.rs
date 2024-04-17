324b7043277173647858885f281bbca897333fb0
Person <localboxcrox@gmail.com>
2024-04-17 16:23:49 -0400

    stru

--- src/boards/mcs51/simulator/state.rs
+++ src/boards/mcs51/simulator/state.rs
@@ -1,10 +1,14 @@
+use crate::U8bits;
+
 
 pub struct State {
-
+    mem: [U8bits; 0xff]
 }
 
 impl State {
     pub fn new() -> State {
-        State {}
+        State {
+            mem: [0; 0xff] // todo: make sure reset vector indeed results in this
+        }
     }
 }
